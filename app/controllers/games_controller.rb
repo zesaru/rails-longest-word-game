90449b67116ef948b9d60f667d4be06979474a52
Cesar Murillo <namiayumis@gmail.com>
2019-05-20 14:19:20 +0900

    rails new

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,39 @@
+class GamesController < ApplicationController
+require 'open-uri'
+require 'json'
+
+  def new
+    @letters = gen_grilla(10)
+  end
+
+  def score
+    @message
+
+    if included?(params[:word], params[:grilla])
+      if english_word?(params[:word])
+        @message = "Congratulations! #{params[:word]} is a valid English Word"
+      else
+        @message = "Sorry but #{params[:word]} does not seem to be a valid English word"
+      end
+    else
+      @message = "Sorry but #{params[:word]} can't be built out of #{params[:grilla]}"
+    end
+    @message
+  end
+
+  private
+
+  def gen_grilla(grilla_size)
+    Array.new(grilla_size) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(word_check, grilla)
+    word_check.chars.all? { |letter| word_check.count(letter) <= grilla.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
